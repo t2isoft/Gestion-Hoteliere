8ea84fb202bbf28bffa0f76cd5e0cadee03a22c2
karl92390 <karl.kelleoglu@laposte.net>
2016-01-07 11:15:31 +0100

    add projet

---
+++ Exe/Archive Java/gestion hoteliere.bat
@@ -0,0 +1,8 @@
+rem Ce fichier de commande a ‚t‚ g‚n‚r‚ par WinDev lors de l'exportation Java du projet <gestion hoteliere>.
+rem Il peut ˆtre utilis‚ pour ex‚cuter votre archive Java (Jar) si le double-clic sur l'archive ne lance pas son ex‚cution.
+rem ATTENTION : Vous devrez adapter la commande ci-dessous pour pouvoir l'utiliser sur une autre machine ou un autre OS.
+
+"C:\Program Files\Java\jdk1.8.0\bin\java.exe" -classpath "gestion hoteliere.jar" GWDPgestion_hoteliere
+
+rem La ligne suivante bloque la fermeture automatique de la console en cas d'erreur : 
+if errorlevel 1 pause
